99d54bf3e5459f1c4ddddd033008bd0a9acb511c
Alex Sanchez <alexanderclaysanchez@gmail.com>
2020-08-24 21:32:45 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,7 @@ def oxford_comma(array)
   if array.length < 3
     array.join(" and ")
   elsif array.length == 3
-    array.join(", ") << "and"
+    array.join(", ")
+    array.last.join("and")
   end
 end
